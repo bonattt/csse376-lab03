786edd6e99d29e91e1807b3cdfbb448d9346ffd8
bonattt <bonattt@rose-hulman.edu>
2015-03-27 14:46:09 -0400

    added first parameterized tests

---
+++ primeFactorization/src/primeNumberGenerator/Generator.java
@@ -0,0 +1,10 @@
+package primeNumberGenerator;
+
+import java.util.ArrayList;
+
+public class Generator {
+	public static ArrayList<Integer> generatePrimes(int i){
+		
+		return null; //new ArrayList<Integer>();
+	}
+}
